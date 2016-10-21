723fc9f6e3e5226c7586cf936c867392559f8e82
Teodor Klimentov <teodor.klimentov@gmail.com>
2016-10-21 23:23:06 +0300

    Adding the basic branch tree printing
    The basic function of this script is to print the local branch
    tree in a pretty form so that a person can understand it.

---
+++ branch.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+#
+#  Simple Git helper that handles branches.
+#  It's main purpouse is to rebase all local branches
+#  over the master branch after a successful pull.
+#
+#  Version: 0.0.1
+#
+
+import subprocess
+import re
+
+
+class Branch:
+    def __init__(self, name, parent=None):
+        self._name = name
+        self._parent = parent
+        self._children = {}
+
+    def __eq__(self, other):
+        if not isinstance(other, Branch):
+            return false
+        return self._name == other._name
+
+    def __str__(self):
+        return self._name
+
+    def __repr__(self):
+        return self._name
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def children(self):
+        return self._children
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
+    def is_parent(self, branch):
+        if not self._parent:
+            return False
+        if self._parent.name == branch.name:
+            return True
+        return self._parent.is_parent(branch)
+
+
+class Git:
+    def show_branch(self):
+        return subprocess.check_output([
+            'git',
+            'show-branch',
+            '--no-color',
+            '--topo-order'
+        ], universal_newlines=True)
+
+
+class Program:
+    def __init__(self):
+        self._git = Git()
+        self._tree = {}
+        self._init_tree()
+
+    def run(self):
+        self._print_tree(self._find_root(), prefix='  ')
+
+    def _find_root(self, branch=None):
+        if not branch:
+            branch = self._tree['master']
+        if not branch.parent:
+            return branch
+        return self._find_root(branch.parent)
+
+    def _print_tree(self, branch, offset='', prefix="->"):
+        print("{0}{1} [{2}]".format(offset, prefix, branch.name))
+        counter = len(branch.children)
+        for _, child in branch.children.items():
+            new_offset = offset.replace("'", ' ').replace('+', '|')
+            if counter == 1:
+                new_offset += "   '"
+            else:
+                new_offset += '   +'
+            self._print_tree(child, offset=new_offset)
+            counter -= 1
+
+    def _init_tree(self):
+        parts = re.split('-+\n', self._git.show_branch())
+        branches = self._build_branches(parts[0])
+
+        self._tree = {branch.name: branch for branch in branches}
+        if len(parts) > 1:
+            self._calculate_tree(branches, parts[1])
+
+    def _build_branches(self, output):
+        result = []
+        for line in output.split('\n'):
+            match = re.match('^.*?\\[(.+?)\\]', line)
+            if not match:
+                continue
+            name = match.group(1)
+            result.append(Branch(name))
+        return result
+
+    def _calculate_tree(self, branches, output):
+        for line in output.split('\n'):
+            match = re.match(
+                '^([\\s*+-]+?)\\s\\[(.+?)(?:[~^]\\d*)?\\]', line)
+            if not match:
+                continue
+            knot, name = match.group(1, 2)
+            for i, c in enumerate(knot):
+                if ' ' == c:
+                    continue
+                if branches[i].name == name:
+                    continue
+                if not branches[i].parent:
+                    branches[i].parent = self._tree[name]
+                if self._tree[name].is_parent(branches[i].parent):
+                    branches[i].parent = self._tree[name]
+        for branch in branches:
+            if not branch.parent:
+                continue
+            branch.parent.children[branch.name] = branch
+#
+#   M A I N
+#
+
+if __name__ == "__main__":
+    Program().run()
