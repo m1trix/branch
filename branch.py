c0eda3fd774c36b12a6029c14785d8684d571a23
Teodor Klimentov <teodor.klimentov@gmail.com>
2016-11-03 23:44:57 +0200

    Add a --more option that renders the commits on each branch

--- branch.py
+++ branch.py
@@ -19,17 +19,12 @@ class Branch:
         self._description = description
         self._parent = parent
         self._children = {}
+        self._commits = []
 
     def __eq__(self, other):
         if not isinstance(other, Branch):
             return false
         return self._name == other._name
-
-    def __str__(self):
-        description = self._description
-        if len(description) > 40:
-            description = description[:37] + '...'
-        return '[{0}] {1}'.format(self._name, description)
 
     @property
     def name(self):
@@ -50,6 +45,10 @@ class Branch:
     @parent.setter
     def parent(self, parent):
         self._parent = parent
+
+    @property
+    def commits(self):
+        return self._commits
 
     def is_parent(self, branch):
         if not self._parent:
@@ -106,22 +105,62 @@ class Git:
         except subprocess.CalledProcessError as e:
             raise GitException(e.output)
 
+    def log(self, fr=None, to=None):
+        try:
+            command = ['git', 'log', '--pretty=oneline']
+            if fr:
+                command.append(fr)
+            if to:
+                command[-1] += '..' + to
+            return subprocess.check_output(command, universal_newlines=True)
+        except subprocess.CalledProcessError:
+            exit('Failed to check git log')
+
+
+class Renderer:
+    COMMIT_FORMAT = '{0}     {1:2}: {2}'
+
+    def render(self, branch):
+        canvas = []
+        self._render(branch, canvas, 3, '', '  ')
+        return canvas
+
+    def _render(self, branch, canvas, context, offset, prefix):
+        for i, key in enumerate(branch.children):
+            child = branch.children[key]
+            new_offset = offset.replace('.', ' ').replace('+', '|')
+            new_offset += '   {}'.format(['+', '.'][i != 1])
+            self._render(child, canvas, -1, new_offset, '->')
+        canvas.append("{0}{1} [{2}]".format(offset, prefix, branch.name))
+        for i, commit in enumerate(branch.commits):
+            if i == context:
+                break
+            canvas.append(self._render_commit(i, commit, offset))
+
+    def _render_commit(self, i, commit, offset):
+        commit_offset = offset.replace('.', '|').replace('+', '|')
+        return Renderer.COMMIT_FORMAT.format(commit_offset, i + 1, commit)
+
 
 class Program:
     def __init__(self):
         self._git = Git()
         self._tree = {}
         self._current_branch = 'master'
-        self._init_tree()
+        self._root = None
 
     def run(self):
         parser = argparse.ArgumentParser(allow_abbrev=False)
         parser.add_argument('-p', '--pull', action='store_true',
                             help='pulls from the remote repository')
+        parser.add_argument('-m', '--more', action='store_true',
+                            help='displays more details in branch tree')
         args = parser.parse_args()
+
+        self._init_tree(args.more)
         if args.pull:
             self._pull_remote_repository()
-        self._print_tree(self._find_root(), prefix='  ')
+        self._print_tree()
 
     def _pull_remote_repository(self):
         with self._git.stash():
@@ -131,33 +170,21 @@ class Program:
             except GitException:
                 exit('Failed to pull from remote repository')
 
-    def _find_root(self, branch=None):
-        if not branch:
-            branch = self._tree['master']
-        if not branch.parent:
-            return branch
-        return self._find_root(branch.parent)
+    def _print_tree(self):
+        rendering = Renderer().render(self._root)
+        for line in rendering:
+            print(line)
 
-    def _print_tree(self, branch, offset='', prefix="->"):
-        print("{0}{1} {2}".format(offset, prefix, str(branch)))
-        counter = len(branch.children)
-        for _, child in branch.children.items():
-            new_offset = offset.replace('`', ' ').replace('+', '|')
-            if counter == 1:
-                new_offset += '   `'
-            else:
-                new_offset += '   +'
-            self._print_tree(child, offset=new_offset)
-            counter -= 1
-
-    def _init_tree(self):
+    def _init_tree(self, details):
         try:
             parts = re.split('-+\n', self._git.show_branch())
             branches = self._build_branches(parts[0])
 
             self._tree = {branch.name: branch for branch in branches}
-            if len(parts) > 1:
-                self._calculate_tree(branches, parts[1])
+            len(parts) > 1 and self._calculate_tree(branches, parts[1])
+
+            self._root = self._find_root()
+            details and self._init_details()
         except GitException as e:
             exit('Failed to calculate branch tree: ' + e.value)
 
@@ -191,6 +218,28 @@ class Program:
             if not branch.parent:
                 continue
             branch.parent.children[branch.name] = branch
+
+    def _find_root(self):
+        for _, branch in self._tree.items():
+            if not branch.parent:
+                return branch
+        raise IllegalStateError('No root branch found')
+
+    def _init_details(self):
+        self._init_commits(self._root)
+
+    def _init_commits(self, branch):
+        if not branch.parent:
+            output = self._git.log(fr=branch.name)
+        else:
+            output = self._git.log(fr=branch.parent.name, to=branch.name)
+        for line in output.strip().split('\n'):
+            if '' == line:
+                continue
+            _, message = line.split(maxsplit=1)
+            branch.commits.append(message)
+        for _, child in branch.children.items():
+            self._init_commits(child)
 
 #
 #   M A I N
